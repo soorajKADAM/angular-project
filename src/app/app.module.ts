59325f4b8bd355f81ed5b857af4201dffa7e9d3a
soorajKADAM <soorajak102@gmail.com>
2022-03-31 17:27:52 +0530

    editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 import { HeaderComponent } from './components/header/header.component';
-
+import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RichTextEditorModule, ToolbarService ,LinkService,ImageService,HtmlEditorService} from '@syncfusion/ej2-angular-richtexteditor'
 @NgModule({
   declarations: [
+
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+     MatFormFieldModule,
+    MatSelectModule,
+    MatTabsModule,
+    MatIconModule,
+    RichTextEditorModule
+    
   ],
-  providers: [],
+  providers: [ToolbarService ,LinkService,ImageService,HtmlEditorService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
